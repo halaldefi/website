918e0b2b7de739e489ccbcebf125c6d5b916b80b
John Hashim <johnhashim@Johns-MacBook-Air.local>
2024-10-04 15:18:34 +0530

    carousal dependencies updated

--- app/(marketing)/terms/page.tsx
+++ app/(marketing)/terms/page.tsx
@@ -84,7 +84,7 @@ export default async function TermsOfUse() {
               >
                 <p>
                   <span className="text-black sm:inline-block">
-                    $invest in Halal
+                    Invest in $HDF
                   </span>
                 </p>
               </Link>
